d862404eabf79be80eb9f0d7735f211a355dbf46
Omid <hassan.barza@sitewards.com>
2021-01-12 13:07:51 +0100

    LRJS R.org refactor main-concepts: use state in timer
    Timer component moved to Time module and timer uses state instead props.

---
+++ src/App/Time/Timer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Timer.css'
+
+class Timer extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render()
+  {
+    return (
+      <h3>{this.state.date.toLocaleTimeString()}</h3>
+    );
+  }
+}
+
+export default Timer;
