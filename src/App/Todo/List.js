8082fea9d1d26690f5eaaa292703a2563d03ad5b
Omid <hassan.barza@sitewards.com>
2021-01-13 22:38:47 +0100

    LRJS R.org feat main-concepts: add list items with a component
    Please note the list item key declared on component composition.
    Keys used within arrays should be unique among their siblings. However they
    don’t need to be globally unique. We can use the same keys when we produce two
    different arrays.
    Keys serve as a hint to React but they don’t get passed to your components. If
    you need the same value in your component, pass it explicitly as a prop with a
    different name.
    
    @link https://reactjs.org/docs/lists-and-keys.html#extracting-components-with-keys

--- src/App/Todo/List.js
+++ src/App/Todo/List.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import Item  from './List/Item';
+import './List.css';
 
 class TodoList extends React.Component
 {
-    items = ['First', 'Second', 'Third', 'Forth', 'Fifth'];
+    items = [
+        {id: '001', value: 'First Item'},
+        {id: '002', value: 'Second Item'},
+        {id: '003', value: 'Third Item'},
+        {id: '004', value: 'Forth Item'},
+        {id: '005', value: 'Fifth Item'},
+    ];
     
     render()
     {
-        let items = this.items.map((item, index) => <li key={index}>{item}</li>);
         return (
-            <ul>{items}</ul>
+            <ul className="todo-list">
+                {this.items.map(
+                    (item) => <Item key={item.id} item={item}/>
+                )}
+            </ul>
         );
     }
 }
